8369c54c34951f158f3c623d2b6b77c1d2b50b4f
v203 <vuyisamr203@gmail.com>
2021-11-30 14:22:22 +0200

    first comm

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const exphbs = require('express-handlebars');
+const bodyParser = require("body-parser");
+const pg = require('pg')
+
+const app = express();
+const Pool = pg.Pool;
+var useSSL = false;
+let local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
+const PORT = process.env.PORT || 3010;
+// const connectionString = process.env.DATABASE_URL || 'postgresql://codex-coder:pg123@localhost:5432/regdb';
+
+// const pool = new Pool({
+//     connectionString,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ partialsDir: "./views/partials", viewPath: './views', layoutsDir: './views/layouts' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log(`Listening at PORT: ${PORT}`);
+})
